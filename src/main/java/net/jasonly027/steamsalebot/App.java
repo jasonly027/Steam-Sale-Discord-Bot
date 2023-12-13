65ec6db9b5e96dab9951098d0a4374977dcc3954
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2023-12-12 21:51:21 -0800

    Project Setup

---
+++ src/main/java/net/jasonly027/steamsalebot/App.java
@@ -0,0 +1,11 @@
+package net.jasonly027.steamsalebot;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class App {
+    public static final Dotenv config = Dotenv.configure().load();
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
