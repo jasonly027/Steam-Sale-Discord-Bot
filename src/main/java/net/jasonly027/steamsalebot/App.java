94fe3e77f73c85785caab537ce49e7def8c78ade
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2023-12-20 18:20:32 -0800

    API Methods: getAppInfo and getAppName

--- src/main/java/net/jasonly027/steamsalebot/App.java
+++ src/main/java/net/jasonly027/steamsalebot/App.java
@@ -1,7 +1,6 @@
 package net.jasonly027.steamsalebot;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import net.jasonly027.steamsalebot.util.database.Database;
 
 public class App {
     public static final Dotenv config = Dotenv.configure().load();
--- src/main/java/net/jasonly027/steamsalebot/steam/SteamApi.java
+++ src/main/java/net/jasonly027/steamsalebot/steam/SteamApi.java
@@ -1,104 +1,74 @@
 package net.jasonly027.steamsalebot.steam;
 
-/*
-Importing steam API
- */
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
+
 public class SteamApi {
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    /*APP POJO*/
-    public static class AppInfo {
-        final String BANNER;
-        final int OG_PRICE;
-        final int SALE_PRICE;
-        final int DISCOUNT_PERCENT;
-        final int REVIEW_COUNT;
-        final String LINK;
+    private SteamApi() {}
 
-        public AppInfo(String BANNER, int OG_PRICE, int SALE_PRICE, int DISCOUNT_PERCENT, int REVIEW_COUNT, String LINK) {
-            this.BANNER = BANNER;
-            this.OG_PRICE = OG_PRICE;
-            this.SALE_PRICE = SALE_PRICE;
-            this.DISCOUNT_PERCENT = DISCOUNT_PERCENT;
-            this.REVIEW_COUNT = REVIEW_COUNT;
-            this.LINK = LINK;
-        }
+    private static HttpClient getClient() {
+        return client;
     }
 
-    /*Takes in App ID of steam game and returns its information as AppInfo POJO*/
-    public static AppInfo getAPPInfo(long APP_ID) {
-        String APP_URL = "https://store.steampowered.com/api/appdetails?appids=" + APP_ID;
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(APP_URL))
+    private static ObjectMapper getMapper() {
+        return objectMapper;
+    }
+
+    private static HttpRequest createHttpGetRequest(String url) {
+        return HttpRequest.newBuilder()
                 .GET()
+                .uri(URI.create(url))
                 .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            //Convert response body to JSON
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(response.body());
-            String STRING_APPID = String.valueOf(APP_ID);
-
-            if(jsonNode.get(STRING_APPID).get("success").asBoolean()){
-                String banner = jsonNode.get(STRING_APPID).get("data").get("header_image").toString();
-                int reviewCount = jsonNode.get(STRING_APPID).get("data").get("recommendations").get("total").asInt();
-
-                int ogPrice = 0;
-                int discountPrice = 0;
-                int discountPercent = 0;
-                //free vs not free
-                if(!jsonNode.get(STRING_APPID).get("data").get("is_free").asBoolean()) {
-                    ogPrice = jsonNode.get(STRING_APPID).get("data").get("price_overview").get("initial").asInt();
-                    discountPrice = jsonNode.get(STRING_APPID).get("data").get("price_overview").get("final").asInt();
-                    discountPercent = jsonNode.get(STRING_APPID).get("data").get("price_overview").get("discount_percent").asInt();
-                }
-                return new AppInfo(banner, ogPrice, discountPrice, discountPercent, reviewCount, APP_URL);
-
-            } else {
-                System.out.println("ERROR: INVALID APP ID");
-            }
-
-        } catch (Exception e) {
-            System.out.println("ERROR: INVALID INPUT");
-        }
-        return null;
     }
 
-    /*Takes in App ID as input as returns the name of the App as a string.*/
-    public static String getName(long APP_ID){
-        String APP_URL = "https://store.steampowered.com/api/appdetails?appids=" + APP_ID;
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(APP_URL))
-                .GET()
-                .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            //Convert response body to JSON
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(response.body());
-            String STRING_APPID = String.valueOf(APP_ID);
-
-            if(jsonNode.get(STRING_APPID).get("success").asBoolean()){
-                return jsonNode.get(STRING_APPID).get("data").get("name").toString();
-            }
-        } catch (Exception e) {
-            System.out.println("ERROR: INVALID INPUT");
+    private static HttpResponse<String> sendHttpRequest(HttpRequest request)
+            throws  IOException, InterruptedException {
+        HttpResponse<String> response = getClient().send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new IOException();
         }
-        return null;
+        return response;
     }
 
-    /*FOR TESTING PURPOSES, feel free to remove*/
-    public static void main(String[] arg){
-        SteamApi.getAPPInfo(440);
-        System.out.println(SteamApi.getName(440));
+    /**
+     * Get a POJO of an app's details, specified by <b>appId</b>. If the query was successful,
+     * i.e., a valid <b>appId</b> was supplied, the POJO's success field will be true. If the
+     * field is false, the other fields will not be set. If the POJO's isFree field is true,
+     * the fields related to price will not be set, otherwise they will be set.
+     * @param appId id of the app to which details are requested
+     * @return a POJO of the app's details
+     * @throws IOException If the response status code was not 200, or if an I/O error
+     * occurs when sending or receiving
+     * @throws InterruptedException if the operation is interrupted
+     */
+    public static AppInfo getAppInfo(long appId) throws IOException, InterruptedException {
+        String appUrl = "https://store.steampowered.com/api/appdetails?appids=" + appId;
+        HttpResponse<String> response = sendHttpRequest(createHttpGetRequest(appUrl));
+
+        return getMapper().readValue(response.body(), AppInfo.class);
+    }
+
+    /**
+     * Get app name of the specified <b>appId</b>.
+     * @param appId id of the app
+     * @return the name of the app or <b>null</b> if the appId was invalid
+     * @throws IOException If the response status code was not 200, or if an I/O error
+     * occurs when sending or receiving
+     * @throws InterruptedException if the operation is interrupted
+     */
+    public static String getAppName(long appId) throws IOException, InterruptedException {
+        String appUrl = "https://store.steampowered.com/api/appdetails?appids=" + appId;
+        HttpResponse<String> response = sendHttpRequest(createHttpGetRequest(appUrl));
+        AppInfo appInfo = getMapper().readValue(response.body(), AppInfo.class);
+
+        return appInfo.getName();
     }
 }
