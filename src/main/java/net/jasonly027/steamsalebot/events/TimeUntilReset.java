bcda8a610c15f46dca112db8575a55fa1263a435
myngbruh <71903484+myngbruh@users.noreply.github.com>
2023-12-23 19:20:20 -0800

    finished TimeUntilReset

--- src/main/java/net/jasonly027/steamsalebot/events/TimeUntilReset.java
+++ src/main/java/net/jasonly027/steamsalebot/events/TimeUntilReset.java
@@ -3,32 +3,34 @@ package net.jasonly027.steamsalebot.events;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.jasonly027.steamsalebot.util.Scheduler;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+
 public class TimeUntilReset extends ListenerAdapter {
 
-    /**/
     @Override
     public void onReady(@NotNull ReadyEvent event){
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-        scheduler.scheduleAtFixedRate(() -> {setStatus(minuteConversionToHours(MINUTE_PLACEHOLDER, event));}, 0, 1, TimeUnit.MINUTES);
+        ScheduledExecutorService scheduler = Scheduler.getScheduler();
+        String convertedTime = convertTime(OnDailyCheck.getTimeTillNextCheckInMinutes());
+        scheduler.scheduleAtFixedRate(setStatus(convertedTime, event), 0, 1, TimeUnit.MINUTES);
     }
 
-    public void setStatus(String timeLeft, ReadyEvent event){
+    private static Runnable setStatus(String timeLeft, ReadyEvent event){
         event.getJDA().getPresence().setActivity(Activity.customStatus(timeLeft + " until reset."));
+        return null;
     }
 
     /*This method takes in an int minutes and converts it into hours and the remaining minutes and returns it as a string.*/
-    public static String minuteConversionToHours(int minutes){
+    private static String convertTime(long minutes){
         if (minutes < 0){
             return "Invalid Time Input";
         }
-        int hours = minutes / 60;
-        int remainingMinutes = minutes % 60;
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
 
         return hours + " hours and " + remainingMinutes + " minutes";
     }
