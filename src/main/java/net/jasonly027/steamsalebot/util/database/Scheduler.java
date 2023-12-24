a5c27a205247d1ca7af4f484fca0d797f4e05062
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2023-12-23 18:09:59 -0800

    Getter for ScheduledExecutor

---
+++ src/main/java/net/jasonly027/steamsalebot/util/database/Scheduler.java
@@ -0,0 +1,14 @@
+package net.jasonly027.steamsalebot.util.database;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class Scheduler {
+    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+
+    private Scheduler() {}
+
+    public static ScheduledExecutorService getScheduler() {
+        return scheduler;
+    }
+}
